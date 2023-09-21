3492c6add3a58eb2eb9cb6e7c76818a7443b2ef3
naimuddin94 <developernaimuddin@gmail.com>
2023-09-21 18:49:14 +0600

    React todo complete project

---
+++ src/context/TodoContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+export const todoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      complete: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => useContext(todoContext);
+
+export const TodoProvider = todoContext.Provider;
